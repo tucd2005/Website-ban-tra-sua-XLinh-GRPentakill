0487a89c95fa1c68017b368b0855eaa4ee1dcf1f
tucd2005 <tucdph51409@gmail.com>
2025-03-20 10:17:51 +0700

    tu-update-header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Login from './component/layout/login'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./component/layout/login";
+import Header from "./component/layout/header";
 
 function App() {
-
-
   return (
     <>
       <Routes>
-        <Route path='/login' element={<Login />}>
-        </Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/header" element={<Header />}></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/component/layout/footer.tsx
+++ src/component/layout/footer.tsx
@@ -1,1 +1,7 @@
+import React from "react";
 
+const footer = () => {
+  return <div>footer</div>;
+};
+
+export default footer;
